1f7ad6b9cc1cf52b51fad73fa105968e304804cf
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-03-26 20:13:14 +0300

    Первый комит

---
+++ player/fsm/attack#2.gd
@@ -0,0 +1,47 @@
+extends StatePlayer
+
+var already_hit: bool
+
+func enter(_msg: Dictionary={}):
+	already_hit = false
+	$"../../Control/L_state".set_text(name)
+	if player.animation.is_flipped_h():
+		player.attack_2_zone.set_scale(Vector2(-1,1))
+	else:
+		player.attack_2_zone.set_scale(Vector2(1,1))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func inner_physics_process(_delta):
+	if not player.is_on_floor():
+		state_machine.change_to("Air")
+	
+	player.animation.play("attack#2")
+
+	if player.animation.get_frame() == 2:
+		player.attack_2_zone.set_monitoring(true)
+	elif player.animation.get_frame() == 4:
+		player.attack_2_zone.set_monitoring(false)
+
+	player.velocity.x = move_toward(player.velocity.x, 0, player.ATTACK_INERTION)
+	player.move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished():
+	if Input.is_action_pressed("ui_attack_2"):
+		state_machine.change_to("Attack#2")
+	elif Input.is_action_pressed("ui_attack"):
+		state_machine.change_to("Attack")
+	elif Input.is_action_pressed("ui_jump"):
+		state_machine.change_to("Air", {do_jump=true})
+	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+			state_machine.change_to("Run")
+	else: 
+		state_machine.change_to("Idle")
+
+
+func _on_attack_2_area_entered(area):
+	if not already_hit:
+		print(area.owner.name)
+		if area.has_method("hit"):
+			area.hit()
+		already_hit = true
+	
---
+++ player/fsm/idle.gd
@@ -0,0 +1,26 @@
+extends StatePlayer
+
+func enter(_msg: Dictionary = {}):
+	player.velocity = Vector2.ZERO
+	$"../../Control/L_state".set_text(name)
+	
+func inner_physics_process(_delta):
+	
+	if not player.is_on_floor():
+		state_machine.change_to("Air")
+		
+	if Input.is_action_just_pressed("ui_attack"):
+		state_machine.change_to("Attack")
+		
+	if Input.is_action_just_pressed("ui_attack_2"):
+		state_machine.change_to("Attack#2")
+		
+	if Input.is_action_just_pressed("ui_jump"):
+		state_machine.change_to("Air", {do_jump = true})
+	
+	if (Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right")):
+		pass
+	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") :
+			state_machine.change_to("Run")
+	
+	player.animation.play("idle")
