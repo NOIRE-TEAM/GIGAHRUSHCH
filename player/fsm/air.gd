1591b3ea9c9373a1ab7967a8a9e293030c977633
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-03-26 20:13:14 +0300

    Первый комит

---
+++ player/fsm/air.gd
@@ -0,0 +1,38 @@
+extends StatePlayer
+
+
+func enter(msg: Dictionary = {}):
+	if msg.has("do_jump"):
+		player.velocity.y = player.JUMP_VELOCITY
+	$"../../Control/L_state".set_text(name)
+	
+func inner_physics_process(delta):
+	
+	if player.velocity.y <0:
+		player.animation.play("jump")
+	elif player.velocity.y ==0:
+		player.animation.play("jump_to_fall")
+	else:
+		player.animation.play("fall")
+	
+	player.velocity.y += player.gravity * delta
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	$"../../Control/L_direction".set_text(str(direction));
+	if direction:
+		player.velocity.x = direction * player.SPEED
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, player.SPEED)
+
+	if direction < 0:
+		player.animation.set_flip_h(true)
+	elif direction > 0:
+		player.animation.set_flip_h(false)
+
+	player.move_and_slide()
+	
+	if player.is_on_floor():
+		if player.velocity.x == 0:
+			state_machine.change_to("Idle")
+		else:
+			state_machine.change_to("Run")
