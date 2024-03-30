1b0aa0174332b0b74828e3eac4934bf24c220b07
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-03-30 13:38:02 +0300

    Второй комит

---
+++ main_world.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func Spawn(param_stroke: String):
+	var spawned_scene = load("res://scenes/game/entities/characters/player/main_char.tscn")
+	var player = spawned_scene.instantiate()
+	var param:Array = param_stroke.to_lower().split("$", false)
+	if param.has("hp"):
+		print("Has Hp")
+		player.change_parametrs_hp(int(param[param.find("hp") + 1]))
+		print(player.hp)
+	else:
+		print("No hp")
+	if param.has("super_name"):
+		print("Has name")
+		player.giga_class = param[param.find("super_name") + 1]
+		print(player.giga_class)
+	player.position = Vector2(150,-200)
+	
+	add_child(player)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_just_pressed("ui_accept")):
+		Spawn("Hp$100$Super_name$plum")
+	if (Input.is_action_just_pressed("ui_focus_next")):
+		if $Player.is_inside_tree():
+			var player = get_node("Player")
+			print(player.transform_to_string())
