cbda375801bbb32767e0c354b6f4ac25f98e314e
NikolaySus <gorkunovnm@gmail.com>
2024-05-09 00:38:20 +0300

    Комичу, а вдруг забыл

--- scenes/game/levels/WorldSaver.gd
+++ scenes/game/levels/WorldSaver.gd
@@ -8,7 +8,7 @@ func to_tilemap(x: int, y: int, content: PackedByteArray):
 	var start_y: int = (y << self.CHUNK_SIZE_Y_POW) - 1
 	for add_y in range(self.CHUNK_SIZE_Y):
 		for add_x in range(self.CHUNK_SIZE_X):
-			if content[(add_y << self.CHUNK_SIZE_Y_POW) + add_x] > 32:
+			if content[(add_y << self.CHUNK_SIZE_Y_POW) + add_x] == 1:
 				placing.set_cell(0, Vector2(start_x + add_x, start_y + add_y), 0, Vector2i.ZERO)
 
 func from_tilemap(x: int, y: int):
@@ -19,10 +19,10 @@ func from_tilemap(x: int, y: int):
 		for add_x in range(self.CHUNK_SIZE_X):
 			var tile = Vector2(start_x + add_x, start_y + add_y)
 			if placing.get_cell_atlas_coords(0, tile).x >= 0:
-				ret.append(33)
+				ret.append(1)
 				placing.erase_cell(0, tile)
 			else:
-				ret.append(32)
+				ret.append(0)
 	return ret
 
 func _ready():
@@ -32,22 +32,17 @@ func _ready():
 
 func _physics_process(delta):
 	self.set_view_center(player.position.x, player.position.y)
-	var str: String = self.which_to_unload()
-	while str.length() != 0:
-		var fields = str.split("\t")
-		var x = int(fields[0])
-		var y = int(fields[1])
-		var content = from_tilemap(x, y)
-		self.unload(str + "\t" + content.get_string_from_ascii())
-		str = self.which_to_unload()
-	str = self.load_another_one()
-	while str.length() != 0:
-		var fields = str.split("\t")
-		var x = int(fields[0])
-		var y = int(fields[1])
-		var content = fields[2].to_ascii_buffer()
-		to_tilemap(x, y, content)
-		str = self.load_another_one()
+	var unload_xy: PackedInt64Array = self.which_to_unload()
+	while !unload_xy.is_empty():
+		var payload = unload_xy.to_byte_array()
+		payload.append_array(from_tilemap(unload_xy[0], unload_xy[1]))
+		self.unload(payload)
+		unload_xy = self.which_to_unload()
+	var payload: PackedByteArray = self.load_another_one()
+	while !payload.is_empty():
+		var fields = payload.slice(0, 16).to_int64_array()
+		to_tilemap(fields[0], fields[1], payload.slice(16))
+		payload = self.load_another_one()
 	if (Input.is_action_just_pressed("ui_attack")):
 		var tile: Vector2 = placing.local_to_map(placing.get_global_mouse_position())
 		placing.set_cell(0, tile, 0, Vector2i.ZERO)
