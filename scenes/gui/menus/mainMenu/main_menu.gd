d4e1f9ff86ee19b82223548ee7f8a4afa6e62253
frosnik <frosnikny@yandex.ru>
2024-06-02 16:29:35 +0300

    Пофиксил баги

--- scenes/gui/menus/mainMenu/main_menu.gd
+++ scenes/gui/menus/mainMenu/main_menu.gd
@@ -8,8 +8,31 @@ func _ready():
 	$Main.show()
 
 func _on_choose_world_pressed():
+	var worlds_container = $Worlds/WorldsChoose/ScrollContainer/VBoxContainer
+	for child in worlds_container.get_children():
+		child.queue_free()
+	var dir = DirAccess.open("res://saves")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir():
+				var butt = Button.new()
+				butt.text = file_name
+				butt.button_group = button_group
+				butt.toggle_mode = true
+				print(butt)
+				#butt.font_size = 20
+				#print(button_group.get_buttons())
+				worlds_container.add_child(butt)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the saves.")
+	
 	$Main.hide()
 	$Worlds.show()
+	$Worlds/NewWorld.hide()
+	$Worlds/WorldsChoose.show()
 
 
 func _on_options_pressed():
@@ -27,8 +50,11 @@ func _on_close_pressed():
 
 
 func _on_play_pressed():
+	#print(button_group.get_pressed_button())
+	#print(button_group.get_buttons())
 	var pressed_button = button_group.get_pressed_button()
-	if pressed_button != null:
+	if pressed_button != null:		
+		GlobalVariables.CurrentWorld = pressed_button.text
 		get_tree().change_scene_to_file("res://scenes/gui/menus/test_menu_scene.tscn")
 
 
@@ -40,3 +66,18 @@ func _on_close_settings_pressed():
 
 func _on_close_settings_mouse_entered():
 	$HoverButton.play()
+
+
+func _on_new_world_pressed():
+	$Worlds/WorldsChoose.hide()
+	$Worlds/NewWorld.show()
+
+
+func _on_create_pressed():
+	GlobalVariables.CurrentWorld = $Worlds/NewWorld/TextEdit.text
+	get_tree().change_scene_to_file("res://scenes/gui/menus/test_menu_scene.tscn")
+
+
+func _on_cancel_pressed():
+	$Worlds/WorldsChoose.show()
+	$Worlds/NewWorld.hide()
