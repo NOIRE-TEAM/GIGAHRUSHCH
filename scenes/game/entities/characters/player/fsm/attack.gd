31c62216ad1c47ee42167402aef3cb634419476a
Qaleka <91965947+Qaleka@users.noreply.github.com>
2024-07-04 00:01:09 +0300

    Чет есть

--- scenes/game/entities/characters/player/fsm/attack.gd
+++ scenes/game/entities/characters/player/fsm/attack.gd
@@ -48,6 +48,6 @@ func _on_attack_2_area_entered(area):
 	if not already_hit:
 		print(area.owner.name)
 		if area.has_method("hit"):
-			area.hit(50, player.position.x)
+			area.hit(50 + player.attack_bonus, player.position.x)
 		already_hit = true
 	
